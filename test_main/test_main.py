9e3b575632bb562ee7fa698b759322652867f5ff
smmtoper <138273906+smmtoper@users.noreply.github.com>
2024-05-31 18:34:55 +0800

    Add files via upload

---
+++ test_main/test_main.py
@@ -0,0 +1,16 @@
+from main import is_even
+
+def test_is_even_with_even_number():
+    assert is_even(4) == True
+
+def test_is_even_with_odd_number():
+    assert is_even(5) == False
+
+def test_is_even_with_zero():
+    assert is_even(0) == True
+
+def test_is_even_with_negative_even_number():
+    assert is_even(-2) == True
+
+def test_is_even_with_negative_odd_number():
+    assert is_even(-3) == False
